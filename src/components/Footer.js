0e5e18848e3a722935ac77adbdb0d7d3dcea283e
Rafal <111435736+RafalRudnik@users.noreply.github.com>
2023-09-21 12:56:12 +0200

    smooth section scroll

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,4 +1,10 @@
 import { useEffect, useState } from 'react';
+import { gsap } from 'gsap';
+import { ScrollTrigger } from 'gsap/ScrollTrigger';
+import { ScrollToPlugin } from 'gsap/ScrollToPlugin';
+
+gsap.registerPlugin(ScrollTrigger);
+gsap.registerPlugin(ScrollToPlugin);
 
 const footerPl = 'projekt i wykonanie';
 const footerGb = 'made and design by';
@@ -6,6 +12,11 @@ const bnc = '{BNC} Rafał Rudnik';
 
 export default function Footer({ polish }) {
 	const [currDate, setCurrDate] = useState('');
+
+	function handleHomeLink(e) {
+		e.preventDefault();
+		gsap.to(window, { duration: 1, scrollTo: { y: '#home', offsetY: 0 } });
+	}
 
 	useEffect(function () {
 		const handleCurrentYear = () => {
@@ -24,7 +35,7 @@ export default function Footer({ polish }) {
 				<a href='https://rafal-rudnik.com/' target='_blank' rel='noreferrer'>
 					{bnc}
 				</a>
-				<a href='#home' onclick="lenis.scrollTo('#home')">
+				<a href='#home' onClick={handleHomeLink}>
 					<i class='ti ti-home-up'></i>
 				</a>
 			</div>
--- src/components/Menu.js
+++ src/components/Menu.js
@@ -1,3 +1,10 @@
+import { gsap } from 'gsap';
+import { ScrollTrigger } from 'gsap/ScrollTrigger';
+import { ScrollToPlugin } from 'gsap/ScrollToPlugin';
+
+gsap.registerPlugin(ScrollTrigger);
+gsap.registerPlugin(ScrollToPlugin);
+
 const workPl = '{Praca}';
 const workGb = '{Work}';
 const aboutPl = '{O nas}';
@@ -6,17 +13,31 @@ const contactPl = '{Kontakt}';
 const contactGb = '{Contact}';
 
 export default function Menu({ polish }) {
+	function handleAboutLink(e) {
+		e.preventDefault();
+		gsap.to(window, { duration: 1, scrollTo: { y: '#about', offsetY: 0 } });
+	}
+
+	function handleWorkLink(e) {
+		e.preventDefault();
+		gsap.to(window, { duration: 1, scrollTo: { y: '#work', offsetY: 100 } });
+	}
+	function handleContactLink(e) {
+		e.preventDefault();
+		gsap.to(window, { duration: 1, scrollTo: { y: '#contact', offsetY: 0 } });
+	}
+
 	return (
 		<div className='menu'>
 			<ul className='menu__list'>
 				<li className='menu__item'>
-					<a href='#about' onclick="lenis.scrollTo('#about')">
+					<a href='#about' onClick={handleAboutLink}>
 						{polish ? aboutPl : aboutGb}
 					</a>
-					<a href='#work' onclick="lenis.scrollTo('#work')">
+					<a href='#work' onClick={handleWorkLink}>
 						{polish ? workPl : workGb}
 					</a>
-					<a href='#contact' onclick="lenis.scrollTo('#contact')">
+					<a href='#contact' onClick={handleContactLink}>
 						{polish ? contactPl : contactGb}
 					</a>
 				</li>
